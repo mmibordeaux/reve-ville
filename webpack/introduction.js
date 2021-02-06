67b0a01291a9a139c4a65dfe7a1a3c6b048d2558
alexisben <alexiben7@gmail.com>
2021-02-06 10:59:10 +0100

    Add mmi loader

--- webpack/introduction.js
+++ webpack/introduction.js
@@ -2,14 +2,22 @@ import { gsap } from 'gsap'
 
 export default () => {
     const introduction = document.querySelector('.js-introduction');
-    gsap.to('#loader', {
-        duration: 0.6,
-        opacity: 0,
-        delay: 2,
-        onComplete: () => {
-            gsap.set('#loader', {display: 'none'});
+    const loader = document.getElementById('loader');
+    const loaderContainer = loader.querySelector('.loader__container');
+
+    gsap.fromTo(loader, {scale: 1}, {
+        duration: 1.4,
+        translateX: '0%',
+        translateY: '0%',
+        scaleX: 0,
+        scaleY: 0,
+        ease: 'power4.inOut',
+        delay: 1,
+        onUpdate: () => {
+            loaderContainer.style.transform = `translate(-50%, -50%) scale(${1 / gsap.getProperty(loader, 'scaleX')}, ${1 / gsap.getProperty(loader, 'scaleY')}) translateZ(0)`
         }
-    })
+    });
+
     gsap.fromTo(introduction.querySelectorAll('.js-animate'), {
         opacity: 0,
         y: 50
